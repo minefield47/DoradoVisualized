7b55efa0b3954e815efa1f73fd3ad6e5264e46e8
minefield47 <arb027@bucknell.edu>
2024-02-28 20:28:25 -0500

    Clarity

--- R/duplex_duplicates.R
+++ R/duplex_duplicates.R
@@ -2,7 +2,7 @@
 #' @author Auden Block
 #' 
 #' @param duplex.df A dataframe of duplex reads
-#' @return A subset of the input duplex dataframe in which a read is used as both a template and a complement. 
+#' @return A subset of the input duplex dataframe in which a read is used as both a template and a complement. Useful for finding false positive duplex reads
 #' 
 #' @examples 
 #' duplex.duplicates <- duplex.duplicates(duplex.df);
